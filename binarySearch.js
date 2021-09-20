0dc87af725600b4b7143f74665376d8be1b2507d
nikhil <ng3152340@gmail.com>
2021-09-20 08:25:30 +0530

    all logic

---
+++ binarySearch.js
@@ -0,0 +1,221 @@
+// 1. Given n distinct numbers, implement a recursive binary search to check the presence of target number k.
+
+function binaryrecursive(arr,low,high,k){
+	if(low>high){
+      return "-1";
+    }
+  var mid = Math.floor((low+high)/2);
+  if(arr[mid]==k){
+    return "1";
+  }else if(arr[mid]<k){
+    return binaryrecursive(arr,mid+1,high,k);
+  }else if(arr[mid]>k){
+  	return binaryrecursive(arr,low,mid-1,k);
+  }
+} 
+
+//  2. You are given an integerN, find the square root of the given integerN. If the integer is not a perfect square, print the floor value of the given integer.
+
+function squareRoot(x) { 
+    if (x == 0 || x == 1){ 
+    return x;
+    }
+    let low = 1; 
+    let high = x; 
+    let ans; 
+    while (low <= high) {
+      
+        let mideum = (low + high) / 2;
+      	let str = ""+mideum;
+      	let strValue = "";
+      	for(let i=0; i<str.length; i++){
+          if(str[i]=="."){
+            break;
+          }else{
+           strValue += str[i];
+          }
+        }
+      	let mid = +strValue;
+        if (mid * mid == x){
+            return mid;
+        }
+      
+        else if(mid * mid < x) {
+            low = mid + 1;
+            ans = mid;
+        } else{ 
+            high = mid-1;
+        }
+    }
+    return ans;
+}
+
+//  3. find Lower bound of a number in logn
+
+function lowerBound (arr,k,low,high) {
+  if (low > high) {
+    return -1
+  }
+  let mid = Math.floor((low + high) / 2)
+
+  if (k < arr[mid]) {
+    return lowerBound(arr,k,low, mid - 1)
+  } else if (k > arr[mid]) {
+    return lowerBound(arr,k, mid + 1, high)
+  } else if(k==arr[mid]){
+    	if(arr[mid]==arr[mid-1]){
+          return lowerBound(arr,k, low, mid)
+        }else{
+         return mid;
+        }
+  }
+}
+
+//  4. Upper Bound in LogN
+
+function lowerBound (arr,k,low,high) { 
+  let mid = Math.floor((low + high) / 2)
+
+  if (k < arr[mid]) {
+    if(k<arr[mid-1]){
+      return lowerBound(arr,k,low, mid - 1)
+    }else{
+     return mid;
+    }
+  } else if (k > arr[mid]) {
+    return lowerBound(arr,k, mid + 1, high)
+  } else if(k==arr[mid]){
+    	if(k<arr[mid+1]){
+          return (mid+1);
+        }else{
+        
+        }
+  }
+}
+
+// 5. You are given n different numbers and an integer k. Write a program that finds number of times k is present in log(n) time complexity.
+
+function lowerBound(arr,k){
+let low = 0;
+  let nas = -1;
+  let high = arr.length-1;
+  while(low<=high){
+    let mid = Math.floor((low+high)/2);
+    if(arr[mid]==k){
+     ans = mid;
+      high = mid-1;
+    }else if(arr[mid]<k){
+     low = mid+1;
+    }else{
+     high = mid-1;
+    }
+  }
+  return ans
+}
+
+function upperBound(arr,k){
+let low = 0;
+  let nas = -1;
+  let high = arr.length-1;
+  while(low<=high){
+    let mid = Math.floor((low+high)/2);
+    if(arr[mid]==k){
+     ans = mid;
+      low = mid+1;
+    }else if(arr[mid]<k){
+     low = mid+1;
+    }else{
+     high = mid-1;
+    }
+  }
+  return ans
+}
+
+//  6. For each test case, you are given N integers and K, your task is to write a program that prints "1" (without quotes) 
+// if there are 2 integers present in the array whose sum is K.Else print "-1"(without quotes).
+
+function pair(arr,k){
+  arr = arr.sort(function(a,b){
+   return a-b;
+  })
+ let i=0; 
+  let j=arr.length-1;
+  while(i<j){
+    if(arr[i]+arr[j]==k){
+     	return "1";
+    }else if(arr[i]+arr[j]<k){
+     i++;
+    }else if(arr[i]+arr[j]>k){
+     j--;
+    }
+  }
+  return "-1";
+}
+
+//  7. Given an array of length n , and it is sorted and rotated at some unknown point, find the minimum element in it.
+
+function findMin(arr,low,high){  
+        if (high < low){
+            return arr[0];
+        }
+        if (high == low){
+            return arr[low];
+        }
+        let mid =Math.floor((low+high)/2)
+         
+        if (mid < high && arr[mid+1] < arr[mid]){
+            return arr[mid+1];
+        }
+        if (mid > low && arr[mid] < arr[mid - 1]){
+            return arr[mid];
+        }
+        if (arr[high] > arr[mid]){
+            return findMin(arr, low, mid-1);
+        }
+     return findMin(arr, mid+1, high);
+}
+
+//  8. Given an array of size n, which is strictly increasing and then strictly decreasing in order. Find out the index (0-based) which is the peak of the array.
+
+function findPeak(arr){
+ let low = 0;
+  let high = arr.length-1;
+  while(low<high){
+    let mid = Math.floor((low+high)/2);
+    if(arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1]){
+      return mid;
+    }else if(arr[mid]>arr[mid-1] && arr[mid]<arr[mid+1]){
+    	low = mid;
+    }else{
+     high = mid;
+    }
+  } 
+}
+
+//  9. Given a sorted(increasing order) array of length n and is rotated by some value beforehand. Find the index of the target element  K in the rotated array in O(logn) time. If not found print -1.
+
+function search(arr,k){
+ let low = 0;
+  let high = arr.length-1;
+  while(low<=high){
+   let mid = Math.floor((low+high)/2);
+    if(arr[mid]==k){
+     return mid;
+    }else if(arr[mid]<=arr[high]){
+    	if(k>arr[mid] && k<=arr[high]){
+          low = mid+1;
+        }else{
+         high = mid-1;
+        }
+    }else{
+     	if(k>=arr[low] && k<arr[mid]){
+         	high = mid-1;
+        }else{
+         low = mid+1;
+         }
+    }
+  }
+  return "-1";
+}
+
+//  10. 
--- linkedListLogic.js
+++ linkedListLogic.js
@@ -1,314 +1,339 @@
 // 1. Insert a node at the Tail
 
-// const LinkedListNode = class {
-//     constructor(nodeData) {
-//         this.data = nodeData;
-//         this.next = null;
-//     }
-// };
+const LinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
 
-// function insertNodeAtTail(head, data) {
-//      let newNode = new LinkedListNode(data)
-//     if (!head) {
-//         head = newNode 
-//         return head
-//     }
-//     let temp  = head
-//     while (temp.next) {
-//         temp = temp.next
-//     }
-//     temp.next = newNode
-//     return head
-// }
+function insertNodeAtTail(head, data) {
+     let newNode = new LinkedListNode(data)
+    if (!head) {
+        head = newNode 
+        return head
+    }
+    let temp  = head
+    while (temp.next) {
+        temp = temp.next
+    }
+    temp.next = newNode
+    return head
+}
 
 
 //  2. Deleting a Node
 
-// const LinkedListNode = class {
-//     constructor(nodeData) {
-//         this.data = nodeData;
-//         this.next = null;
-//     }
-// };
+const LinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
 
-// function deleteNode(head, position) {
-//    let temp = head;
-//   for(let i=1; i<position; i++){
-//    temp = temp.next;
-//   }
-//   temp.next = temp.next.next;
-//   return head;
-// }
+function deleteNode(head, position) {
+   let temp = head;
+  for(let i=1; i<position; i++){
+   temp = temp.next;
+  }
+  temp.next = temp.next.next;
+  return head;
+}
 
 
 //  3. Insert at a specific position
 
-// const LinkedListNode = class {
-//     constructor(nodeData) {
-//         this.data = nodeData;
-//         this.next = null;
-//     }
-// };
+const LinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
 
-// function insertNodeAtPosition(head, data, position) {
-//   let newNode = new LinkedListNode(data);
-//   if(position==0){
-//    newNode.next = head;
-//     return newNode;
-//   }
-//     let temp = head;
-//   for(let i=1; i<position; i++){
-//    temp = temp.next;
-//   }
-//   let old = temp;
-//   newNode.next = old.next;
-//   temp.next = newNode;
-//   return head;
-// }
+function insertNodeAtPosition(head, data, position) {
+  let newNode = new LinkedListNode(data);
+  if(position==0){
+   newNode.next = head;
+    return newNode;
+  }
+    let temp = head;
+  for(let i=1; i<position; i++){
+   temp = temp.next;
+  }
+  let old = temp;
+  newNode.next = old.next;
+  temp.next = newNode;
+  return head;
+}
 
 
 //  4. Find Nth node from the end
 
-// const LinkedListNode = class {
-//     constructor(nodeData) {
-//         this.data = nodeData;
-//         this.next = null;
-//     }
-// };
+const LinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
 
-// function nth_node(head,n){
-//   if (n <= 0) {
-//     return head;
-//   }
+function nth_node(head,n){
+  if (n <= 0) {
+    return head;
+  }
 	
-//  let length = 0;
-//   let temp = head;
+ let length = 0;
+  let temp = head;
   
-//   while(temp!==null){
-//    temp = temp.next;
-//     length++;
-//   }
+  while(temp!==null){
+   temp = temp.next;
+    length++;
+  }
   
-//   let step = length-n;
-//   temp = head;
+  let step = length-n;
+  temp = head;
   
-//   for(let i=0; i<step;i++){
-//    temp = temp.next;
-//   }
-//   return temp.data;
-// }
+  for(let i=0; i<step;i++){
+   temp = temp.next;
+  }
+  return temp.data;
+}
 
 
 // 5. Reverse the Linked List
 
-// const LinkedListNode = class {
-//     constructor(nodeData) {
-//         this.data = nodeData;
-//         this.next = null;
-//     }
-// }
+const LinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+}
 
-// function reverse(head) {
-//   let prev = null;
-//   let next = null;
-//     let temp = head;
-//   while(temp!=null){
-//   	next = temp.next;
-//     temp.next = prev;
-//     prev = temp;
-//     temp = next;
-//   }
-//   return prev;
-// }
+function reverse(head) {
+  let prev = null;
+  let next = null;
+    let temp = head;
+  while(temp!=null){
+  	next = temp.next;
+    temp.next = prev;
+    prev = temp;
+    temp = next;
+  }
+  return prev;
+}
 
 //  6. Return a middle node of the linked list.
 
-// const LinkedListNode = class {
-//     constructor(nodeData) {
-//         this.data = nodeData;
-//         this.next = null;
-//     }
-// };
+const LinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
 
-// var middleNode = function(head) {
-// let slow = head;
-//   let fast = head;
+var middleNode = function(head) {
+let slow = head;
+  let fast = head;
   
-//   while(fast!==null && fast.next!==null){
-//    slow = slow.next;
-//     fast = fast.next.next;
-//   }
+  while(fast!==null && fast.next!==null){
+   slow = slow.next;
+    fast = fast.next.next;
+  }
   
-//   return slow.data;
-// };
+  return slow.data;
+};
 
 //  7. Given a linked list, rotate the list to the right by k places.
 
 
 // 8. Givenhead, the head of a linked list, determine if the linked list has a cycle in it.
 
-// const LinkedListNode = class {
-//     constructor(nodeData) {
-//         this.data = nodeData;
-//         this.next = null;
-//     }
-// };
+const LinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
 
-// var hasCycle = function(head) {
-//      var s = new Set();
+var hasCycle = function(head) {
+     var s = new Set();
   
-//         while (head != null) {
-//             if (s.has(head)){
-//                 return true;
-//             }
-//             s.add(head);
-//             head = head.next;
-//         }
+        while (head != null) {
+            if (s.has(head)){
+                return true;
+            }
+            s.add(head);
+            head = head.next;
+        }
  
-//         return false;
-// };
+        return false;
+};
 
 
 // 9. Merge two sorted linked lists and return it as a new sorted list
 
-// const ListNode = class {
-//     constructor(nodeData) {
-//         this.val = nodeData;
-//         this.next = null;
-//     }
-// };
+const ListNode = class {
+    constructor(nodeData) {
+        this.val = nodeData;
+        this.next = null;
+    }
+};
 
-// var mergeTwoLists = function(l1, l2) {
-// 	let p1 = l1;
-//    let p2 = l2;
+var mergeTwoLists = function(l1, l2) {
+	let p1 = l1;
+   let p2 = l2;
   
-//   let dummyNode = new ListNode(-1);
-//   let p3 = dummyNode;
+  let dummyNode = new ListNode(-1);
+  let p3 = dummyNode;
   
-//   while(p1!=null && p2!=null){
-//      if(p1.val<=p2.val){
-//        p3.next = p1;
-//        p1 = p1.next;
-//      }else{
-//      	p3.next = p2;
-//        p2 = p2.next;
-//      }
-//     p3 = p3.next;
-//   }
+  while(p1!=null && p2!=null){
+     if(p1.val<=p2.val){
+       p3.next = p1;
+       p1 = p1.next;
+     }else{
+     	p3.next = p2;
+       p2 = p2.next;
+     }
+    p3 = p3.next;
+  }
   
-//   if(p1!=null){
-//    p3.next = p1;
-//   }
-//   if(p2!=null){
-//   p3.next = p2;
-//   }
+  if(p1!=null){
+   p3.next = p1;
+  }
+  if(p2!=null){
+  p3.next = p2;
+  }
   
-//   return dummyNode.next;
-// };
+  return dummyNode.next;
+};
 
 
 // 10. Given a singly linked list, determine if it is a palindrome.
 
-// const LinkedListNode = class {
-//     constructor(nodeData) {
-//         this.data = nodeData;
-//         this.next = null;
-//     }
-// };
+const LinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
 
-// var isPalindrome = function (head) {
-//     var slow = head;
-//         var ispalin = true;
-//         var stack = [];
+var isPalindrome = function (head) {
+    var slow = head;
+        var ispalin = true;
+        var stack = [];
  
-//         while (slow != null) {
-//             stack.push(slow.data);
-//             slow = slow.next;
-//         }
+        while (slow != null) {
+            stack.push(slow.data);
+            slow = slow.next;
+        }
  
-//         while (head != null) {
+        while (head != null) {
  
-//             var i = stack.pop();
-//             if (head.data == i) {
-//                 ispalin = true;
-//             } else {
-//                 ispalin = false;
-//                 break;
-//             }
-//             head = head.next;
-//         }
-//         return ispalin;
-// };
+            var i = stack.pop();
+            if (head.data == i) {
+                ispalin = true;
+            } else {
+                ispalin = false;
+                break;
+            }
+            head = head.next;
+        }
+        return ispalin;
+};
 
 
 // 11. Given a linked list, rotate the list to the right by k places, where k isnon-negative.
 
-// const LinkedListNode = class {
-//     constructor(nodeData) {
-//         this.data = nodeData;
-//         this.next = null;
-//     }
-// };
+const LinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
 
-// var rotateRight = function(head, k) {
-//   if(head==null){
-//    return head;
-//   }  
+var rotateRight = function(head, k) {
+  if(head==null){
+   return head;
+  }  
   
-//   var tmp = head;
-//   var len = 1;
-//   while (tmp.next != null) {
-//         tmp = tmp.next;
-//         len++;
-//      }
+  var tmp = head;
+  var len = 1;
+  while (tmp.next != null) {
+        tmp = tmp.next;
+        len++;
+     }
   
-//   if(k>len){
-//    k = k%len;
-//   }
-//   k = len-k;
+  if(k>len){
+   k = k%len;
+  }
+  k = len-k;
   
-//   if(k==0 || k==len){
-//    return head;
-//   }
+  if(k==0 || k==len){
+   return head;
+  }
   
-//   var current = head;
-//    var cnt = 1;
-//     while (cnt < k && current != null) {
-//          current = current.next;
-//          cnt++;
-//      }
+  var current = head;
+   var cnt = 1;
+    while (cnt < k && current != null) {
+         current = current.next;
+         cnt++;
+     }
   
-//   if (current == null)
-//        return head;
+  if (current == null)
+       return head;
   
-//   var kthnode = current;
-//   tmp.next = head;
-//   head = kthnode.next;
-//   kthnode.next = null;
-//   return head; 
-// };
+  var kthnode = current;
+  tmp.next = head;
+  head = kthnode.next;
+  kthnode.next = null;
+  return head; 
+};
 
 // 12. A number is given represented in the form of a linked list. Add one to it.
 
+const LinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+
+var addOneToLinkedList = function(head) {
+    let prev = null;
+  let next = null;
+    let temp = head;
+  while(temp!=null){
+  	next = temp.next;
+    temp.next = prev;
+    prev = temp;
+    temp = next;
+  }
+    let current = prev;
+    current.data = current.data+1;
+    let carry = 0;
+  
+  let newTemp;
+    while(current!=null && (current.data>9 || carry>0)){
+      newTemp = current;
+      current.data = current.data+carry;
+      carry = current.data/10;
+      current.data = current.data%10;
+      current = current.next;
+    }
+    if(carry>0){
+     newTemp.next = new LinkedListNode(carry)
+    }
+  
+   let newPrev = null;
+  let newNext = null;
+    let secoundTemp = current;
+  while(secoundTemp!=null){
+  	newNext = secoundTemp.next;
+    secoundTemp.next = newPrev;
+    newPrev = secoundTemp;
+    secoundTemp = newNext;
+  }
+  return newPrev;
+};
 
 
-
-// 13. Amit went down to the Samosa street to have some. But he only has K units of money with him. There are N shops on the street and unfortunately, all of them have only one samosa remaining. You are also given an array A, where Ai is the cost of a samosa on the i'th shop.
-// Find the maximum samosas that Amit can eat.
-
-// function findSamosa(arr,k){
-// 	arr = arr.sort(function(a,b){
-//      return a-b;
-//     })
-//   let count = 0;
-//   for(let i=0; k>0; i++){
-//     if(k>=arr[i]){
-//      count++;
-//       k = k-arr[i];
-//     }else{
-//      break;
-//     }
-//   }
-//   return count;
-// }
 
